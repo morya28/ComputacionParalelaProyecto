d18a8fc403a30982bd0edbd9d0236ffb5c61b3b8
morya28 <moryacm28@gmail.com>
2022-05-11 22:04:48 -0500

    Enhanced Django app.

--- django_app_docker_image/Django_app/telemetryhub/mongodb_config/mongodb_utils.py
+++ django_app_docker_image/Django_app/telemetryhub/mongodb_config/mongodb_utils.py
@@ -1,26 +1,22 @@
+import json
 from copy import deepcopy
 
 
 PAYLOAD = {
     'farm_job_id': '',
-    'service': '',
-    'env_type': '',
-    'family': '',
-    'test_case': '',
-    'json_type': '',
-    'date': '',
     'json_data': ''
 }
 
 
 def get_payload(farm_job_id):
-    """ """
     payload = deepcopy(PAYLOAD)
     if farm_job_id == 'foobar':
-        payload['json_data'] = 'DIOOOOOOO!'
+        payload['farm_job_id'] = farm_job_id
+        json_data = '{"name": "DIOOOOOOO!", "ora": "ora", "ora1": "ora", "ora2": "ora", "ora3": "ora", "ora4": {"foo": "bar"}}'
+        json_data = json.loads(json_data)
+        payload['json_data'] = json.dumps(json_data, indent=4)
     return payload
 
 
 def get_empty_payload():
-    """ """
     return deepcopy(PAYLOAD)
--- django_app_docker_image/Django_app/telemetryhub/views.py
+++ django_app_docker_image/Django_app/telemetryhub/views.py
@@ -1,30 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from datetime import datetime
-
 from telemetryhub.mongodb_config import mongodb_utils
 
 
 def telemetry_hub(request, farm_job_id="", service="", env_type="", family="",
                   test_case="", json_type="", json_data=None):
-    """ """
     parameters = {
         'farm_job_id': farm_job_id,
-        'service': service,
-        'env_type': env_type,
-        'family': family,
-        'test_case': test_case,
-        'json_type': json_type,
-        'date': _get_date(),
         'json_data': json_data
     }
     return render(request, 'telemetry_hub.html', parameters)
-
-
-def _get_date():
-    actual_date = datetime.now()
-    return actual_date.strftime("%m/%d/%Y %H:%M:%S")
 
 
 def search_id(request):
